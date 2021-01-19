c215233160c0cb02fff10621f9d4cf034aff83d4
lacmoore <77640102+lacmoore@users.noreply.github.com>
2021-01-19 10:52:38 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,12 +4,14 @@ function compute()
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = parseInt(document.getElementById("years").value);
-   checkValue();
+   
 var interest = principal * years * rate/100
 var actual_years = (new Date()).getFullYear() + years;
 var result = document.getElementById("result");
-result.innerHTML = "If you deposit <mark>"+principal+"</mark>,at an interest rate of <mark>"+ rate+"%</mark>. You will receive an amount of <mark>"+interest+"</mark>, in the year <mark>"+actual_years+"</mark>";
-
+    if(checkValue())
+    {
+        result.innerHTML = "If you deposit <mark>"+principal+"</mark>,at an interest rate of <mark>"+ rate+"%</mark>. You will receive an amount of <mark>"+interest+"</mark>, in the year <mark>"+actual_years+"</mark>";
+    }
 
 }
 function writePercentage(){
@@ -21,5 +23,7 @@ function checkValue(){
         if( document.getElementById("principal").value=="" || parseInt(document.getElementById("principal").value) < 1){
         alert("Enter a positive number");
         document.getElementById("principal").focus();
+            return false;
     }
+    return true;
 }
