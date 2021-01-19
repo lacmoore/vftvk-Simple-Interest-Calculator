82f1ecad8aea73f0a9551f90f93c47c5830e07c4
LaChanda Moore <lmoore@dezign-concepts.com>
2021-01-19 00:45:57 +0000

    changes made for Final Project

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
+
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = parseInt(document.getElementById("years").value);
+    console.log(principal);
+    console.log(rate);
+    console.log(years);
+var interest = principal * years * rate/100
+var actual_years = (new Date()).getFullYear() + years;
+var result = document.getElementById("result");
+result.innerHTML = "If you deposit <b>"+principal+"</b>,at an interest rate of <b>"+ rate+"%</b>. You will receive an amount of <b>"+interest+"</b>, in the year <b>"+actual_years+"</b>";
+
+
 }
-        
+function writePercentage(){
+   document.getElementById("percentage").innerText = document.getElementById("rate").value + "%";
+}
+    
+function checkValue(){
+    if(parseInt(document.getElementById("principal").value) < 1){
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+    }
+}
